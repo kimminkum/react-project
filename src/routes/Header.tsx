4cd0c4d7460401fc90e56c9a945329fd10df8c6e
kimminkum <h24breaker@naver.com>
2023-07-07 17:52:04 +0900

    headering

--- src/routes/Header.tsx
+++ src/routes/Header.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 
 import imgLogo from "../image/logo.png";
 import styled from "styled-components";
@@ -11,31 +12,114 @@ interface HeaderProps {
   onToggle: () => void;
 }
 
-const HeaderPc = styled.div`
+const HeaderPc = styled.div<{ active: boolean }>`
   width: 100%;
-  padding: 20px 0;
-  max-width: 1200px;
+  padding: 10px 0;
   position: absolute;
   top: 0;
+  display: ${(props) => (props.active ? "block" : "none")};
+`;
+const InnerBox = styled.div`
+  width: 1200px;
+  margin: 0 auto;
+  align-items: center;
+`;
+const HeaderMb = styled.div<{ active: boolean }>`
+  width: 100%;
+  padding: 20px;
+  position: absolute;
+  top: 0;
+  display: ${(props) => (props.active ? "flex" : "none")};
 `;
 
 const Div = styled.div``;
 
-const HeaderImg = styled.img`
-  width: 100%;
+const HeaderImgPc = styled.img`
+  width: 260px;
+`;
+const HeaderImgMb = styled.img`
+  width: 180px;
 `;
 
-const Pcnav = styled.ul``;
-const PcnavList = styled.li``;
+const Pcnav = styled.ul`
+  padding: 8px 0;
+  overflow: hidden;
+  align-items: center;
+  height: 100%;
+`;
+const PcnavList = styled.li`
+  position: relative;
+  width: 130px;
+  margin: 0 30px;
+  border-radius: 20px;
+  padding: 12px 0;
+  text-align: center;
+  color: #007d7c;
+  &:hover {
+    background-color: #def0f2;
+  }
+`;
 
 const Header: React.FC<HeaderProps> = ({ windowWidth, onToggle }) => {
+  const [mbButton, setMbButton] = useState<boolean>(false);
+  const [hideHeader, setHideHeader] = useState<boolean>(true);
+
+  useEffect(() => {
+    // 이전 스크롤 위치를 저장하는 변수
+    let prevScrollPos = window.pageYOffset;
+
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      const isTop = currentScrollPos === 0;
+
+      if (isTop) {
+        setHideHeader(false);
+      } else {
+        setHideHeader(true);
+      }
+
+      prevScrollPos = currentScrollPos;
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div>
       {windowWidth > 1200 && (
-        <HeaderPc className="flex_sb">
+        <HeaderPc active={hideHeader}>
+          <InnerBox className="flex_sb">
+            <Link to="/">
+              <div>
+                <HeaderImgPc src={imgLogo} alt="" />
+              </div>
+            </Link>
+
+            <div className="flex_end">
+              <Pcnav className="flex_end">
+                <PcnavList>
+                  <Link to="/">서비스 소개</Link>
+                </PcnavList>
+                <PcnavList>
+                  <Link to="/">회사 소개</Link>
+                </PcnavList>
+                <PcnavList>
+                  <Link to="/">무료 견적 받아보기</Link>
+                </PcnavList>
+              </Pcnav>
+            </div>
+          </InnerBox>
+        </HeaderPc>
+      )}
+      {windowWidth < 1200 && (
+        <HeaderMb active={hideHeader} className="flex_sb">
           <Link to="/">
             <div>
-              <HeaderImg src={imgLogo} alt="" />
+              <HeaderImgMb src={imgLogo} alt="" />
             </div>
           </Link>
 
@@ -52,7 +136,7 @@ const Header: React.FC<HeaderProps> = ({ windowWidth, onToggle }) => {
               </PcnavList>
             </Pcnav>
           </div>
-        </HeaderPc>
+        </HeaderMb>
       )}
     </div>
   );
